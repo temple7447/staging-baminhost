6fbaf0a0d270c139018911eba9d323fb4f75dba3
Temple7447 <starukido@gmail.com>
2025-09-17 13:07:26 +0100

    feat: Update API base URL to point to production server

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,2 +1,2 @@
-export const BASE_API_URL = 'http://localhost:4000';
-// export const BASE_API_URL = 'https://bamihustle-backend.onrender.com'; --- IGNORE ---
+// export const BASE_API_URL = 'http://localhost:4000';
+export const BASE_API_URL = 'https://bamihustle-backend.onrender.com';
